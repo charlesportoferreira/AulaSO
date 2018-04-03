6e7f6c74d0e76bd7fb5f1175881b627d74452fa5
charles <charlesportoferreira@gmail.com>
2018-04-03 17:12:49 -0300

    update indentation

--- AppParalelo.java
+++ AppParalelo.java
@@ -2,10 +2,10 @@ import java.util.concurrent.CountDownLatch;
 
 public class AppParalelo{
 
-	public static void main(String args[]){
+  public static void main(String args[]){
     CountDownLatch contador = new CountDownLatch(2);
 
-	  double [][]dados = new double[10000][10000];
+    double [][]dados = new double[10000][10000];
     Worker w1 = new Worker(dados, 0, 5000, contador);
     Worker w2 = new Worker(dados, 5000, dados.length, contador);
     Thread t1 = new Thread(w1);
@@ -14,11 +14,11 @@ public class AppParalelo{
     t1.start();
     t2.start();
 	 
-   try{
-    contador.await();
-   }catch(Exception ex){
-    System.out.println("Algo errado ocorreu");
-   }
+    try{
+      contador.await();
+    }catch(Exception ex){
+      System.out.println("Algo errado ocorreu");
+    }
 
     double soma = 0;
     for(int i = 0; i < dados.length; i++)
